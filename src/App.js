a6f6a8aa6db78d1bd58340393278d8fc34c3a10c
ississam <issam_elyazri@yahoo.fr>
2020-10-02 20:39:02 +0100

    front-end-template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Loading from './Landing';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Landing from './Landing';
+function App() {
+  return (
+   <div>
+ <Landing />
+    </div>
+  );
+}
+
+export default App;
